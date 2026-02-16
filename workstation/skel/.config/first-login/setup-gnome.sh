90f43382d7567f76a966b2418ecfebfa77ac44fe
nick <noobping@users.noreply.github.com>
2026-02-16 17:18:58 +0100

    Terminal shortcut

--- workstation/skel/.config/first-login/setup-gnome.sh
+++ workstation/skel/.config/first-login/setup-gnome.sh
@@ -3,7 +3,18 @@ set -e
 
 BASE="$HOME/.config/first-login"
 dconf load /org/gnome/shell/extensions/ < "$BASE/gnome-extensions-settings.dconf"
-dconf load /org/gnome/shell/ < "$BASE/gnome-shell-settings.dconf"
-gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Control><Alt>t']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys terminal-command 'ptyxis --new-window'
+# dconf load /org/gnome/shell/ < "$BASE/gnome-shell-settings.dconf"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+"['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
+name 'Terminal'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
+command 'ptyxis --new-window'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
+binding '<Control><Alt>t'
+
 touch "$HOME/.config/first-login/.done"
