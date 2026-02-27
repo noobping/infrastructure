9dba8a66df73685670441817dc9d18baba79a31a
nick <noobping@users.noreply.github.com>
2026-02-27 14:49:26 +0100

    remove no passwd

--- nas/todo.sh
+++ nas/todo.sh
@@ -15,3 +15,8 @@ if [ -e /dev/md/raid1 ]; then
     echo "Adding TPM to raid 1"
     sudo systemd-cryptenroll --tpm2-device=auto /dev/md/raid1
 fi
+
+if [ -e "/etc/sudoers.d/$USER" ]; then
+    sudo sed -i 's/\bNOPASSWD:\b//g' "/etc/sudoers.d/$USER"
+    sudo visudo -cf "/etc/sudoers.d/$USER"
+fi
