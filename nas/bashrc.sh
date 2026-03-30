759c6380da473fb229029f699ddc88728a5407a5
nick <noobping@users.noreply.github.com>
2026-03-30 14:09:49 +0200

    user with password

--- nas/bashrc.sh
+++
@@ -1,7 +0,0 @@
-
-if [ -f /usr/bin/todo ]; then
-   status="$(passwd -S "$USER" 2>/dev/null | awk '{print $2}')"
-   if [ "$status" = "NP" ] || [ "$status" = "L" ]; then
-      /usr/bin/todo
-   fi
-fi
