9f7f6df258192df90a20a36f0f691375287c60aa
nick <noobping@users.noreply.github.com>
2026-04-22 23:23:16 +0200

    remove old sudoers helper

--- nas/todo.sh
+++ nas/todo.sh
@@ -6,7 +6,6 @@ if [ "$status" = "NP" ] || [ "$status" = "L" ]; then
     echo "Set password for $USER"
     sudo passwd $USER
     sudo passwd -l root
-    sudo systemctl disable getty@tty1.service
 fi
 
 if [ -e /dev/md/raid0 ]; then
@@ -17,9 +16,4 @@ fi
 if [ -e /dev/md/raid1 ]; then
     echo "Adding TPM to raid 1"
     sudo systemd-cryptenroll --tpm2-device=auto /dev/md/raid1
-fi
-
-if [ -e "/etc/sudoers.d/$USER" ]; then
-    sudo sed -i 's/\bNOPASSWD:\b//g' "/etc/sudoers.d/$USER"
-    sudo visudo -cf "/etc/sudoers.d/$USER"
 fi
